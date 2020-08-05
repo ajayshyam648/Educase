bf6689c9d5e0d723c47b534a586bc6e80c0da4ea
Ajay Shyam <as.modi.1997@gmail.com>
2020-08-05 19:04:52 +0530

    Updates

---
+++ precache-manifest.3deac9c7a8b96b1f56e66947b9930e23.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15df2fac9aea4fbfb8373350ebc8de8b",
+    "url": "/Educase/index.html"
+  },
+  {
+    "revision": "901b09302ef5cb6da114",
+    "url": "/Educase/static/js/2.d923cc44.chunk.js"
+  },
+  {
+    "revision": "ccf284d35e5918e138dc",
+    "url": "/Educase/static/js/main.149f09bf.chunk.js"
+  },
+  {
+    "revision": "2bb081907384aaa0d5c2",
+    "url": "/Educase/static/js/runtime~main.29a822fb.js"
+  }
+]);
